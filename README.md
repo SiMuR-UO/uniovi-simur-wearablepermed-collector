ab8669ceb31ac95a82a68ba1292df82d978793b7
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2025-05-27 19:34:10 +0200

    last commit

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Copy all files from one folder to another one excluding some file extensions.
 Copy all files from folder `/media/simur/maxone/COMPLETOS` to folder `/home/simur/git/uniovi-simur-wearablepermed-data` except images (*.JPG) and movies (*.MOV) files:
 
 ```
-$ python3 copier.py \
+$ python3 collector.py \
 -sr '/media/simur/maxone/COMPLETOS' \
 -dr '/home/simur/git/uniovi-simur-wearablepermed-data' \
 -ex '.MOV, .JPG'
---
+++ checker.py
@@ -0,0 +1,85 @@
+import argparse
+import os
+import shutil
+import sys
+import logging
+from tqdm import tqdm
+
+__author__ = "Miguel Angel Salinas Gancedo"
+__copyright__ = "Simur"
+__license__ = "MIT"
+
+_logger = logging.getLogger(__name__)
+
+files_to_copy = []
+
+def parse_extensions(ext_string):
+    # Split by comma, strip whitespace, ensure each starts with dot, and make a set
+    return {ext.strip() if ext.strip().startswith('.') else '.' + ext.strip()
+            for ext in ext_string.split(',')}
+
+def parse_args(args):
+    """Parse command line parameters
+
+    Args:
+      args (List[str]): command line parameters as list of strings
+          (for example  ``["--help"]``).
+
+    Returns:
+      :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    parser = argparse.ArgumentParser(description="BIN to CSV Converter")
+
+    parser.add_argument(
+        "-sr",
+        "--source-root",
+        required=True,
+        dest="source_root",
+        help="Source root folder",
+    )            
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action="store_const",
+        const=logging.INFO,
+    )
+    parser.add_argument(
+        "-vv",
+        "--very-verbose",
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action="store_const",
+        const=logging.DEBUG,
+    )
+
+    return parser.parse_args(args)
+
+def setup_logging(loglevel):
+    """Setup basic logging
+
+    Args:
+      loglevel (int): minimum loglevel for emitting messages
+    """
+    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
+    logging.basicConfig(
+        level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+def check_files(args):
+    for root, dirs, files in os.walk(args.source_root):
+        for file in files:
+            source_file = os.path.join(root, file)
+
+    return files_to_copy
+
+_logger.info("Starting checker ...")
+
+args = parse_args(sys.argv[1:])
+setup_logging(args.loglevel)
+
+_logger.info("Check files ...")
+check_files(args)
+
+_logger.info("Ending checker ...")
